b501ffa447b4a3ede4d6e92f951701e21402d915
Gift Nnko <nnkogift@gmail.com>
2024-11-14 17:20:46 +0300

    fix: secure storage
    Set the secure storage to use secure shared preferences

--- lib/src/services/auth_service/secure_storage.dart
+++ lib/src/services/auth_service/secure_storage.dart
@@ -3,7 +3,10 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class D2SecureStorageService {
-  FlutterSecureStorage storage = const FlutterSecureStorage();
+  FlutterSecureStorage storage = const FlutterSecureStorage(
+      aOptions: AndroidOptions(
+    encryptedSharedPreferences: true,
+  ));
 
   Future<String?> get(String key) async {
     return await storage.read(key: key);
