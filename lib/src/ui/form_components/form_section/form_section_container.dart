a05db68bfe4b7d6b97549b47b8f3ada77e4d12aa
Dave <davistibz@gmail.com>
2025-01-10 13:11:26 +0300

    fix(collapsable sections): fixes
    - Improved ui of collapsable form sections

--- lib/src/ui/form_components/form_section/form_section_container.dart
+++ lib/src/ui/form_components/form_section/form_section_container.dart
@@ -49,67 +49,76 @@ class _FormSectionContainerState extends State<FormSectionContainer>
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: InkWell(
+            onTap: () {
+              if (widget.isCollapsable) {
+                setState(() {
+                  collapsed = !collapsed;
+                  if (!collapsed) {
+                    _iconController!.forward();
+                  } else {
+                    _iconController!.reverse();
+                  }
+                });
+              }
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                widget.section.title != null
-                    ? Text(
-                        widget.section.title!,
-                        style: TextStyle(
-                          color: widget.color,
-                          fontSize: 24,
-                        ),
-                      )
-                    : Container(),
-                const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
-                widget.section.subtitle != null
-                    ? Text(
-                        widget.section.subtitle!,
-                        style: const TextStyle(
-                            color: Colors.blueGrey, fontSize: 16),
-                      )
-                    : Container(),
-              ],
-            ),
-            Visibility(
-              visible: !widget.hasError && widget.isCollapsable,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: InkWell(
-                  onTap: () {
-                    setState(() {
-                      collapsed = !collapsed;
-                      if (!collapsed) {
-                        _iconController!.forward();
-                      } else {
-                        _iconController!.reverse();
-                      }
-                    });
-                  },
-                  child: RotationTransition(
-                    turns: Tween<double>(
-                      begin: 0.0,
-                      end: 0.5,
-                    ).animate(_iconController!),
-                    child: Icon(
-                      Icons.keyboard_arrow_down,
-                      color: Colors.grey[900],
-                      size: 30,
+                Expanded(
+                  child: Column(
+                    children: [
+                      widget.section.title != null
+                          ? Text(
+                              widget.section.title!,
+                              style: TextStyle(
+                                color:
+                                    widget.hasError ? Colors.red : widget.color,
+                                fontSize: 24,
+                              ),
+                            )
+                          : Container(),
+                      const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 2.0)),
+                      widget.section.subtitle != null
+                          ? Text(
+                              widget.section.subtitle!,
+                              style: const TextStyle(
+                                  color: Colors.blueGrey, fontSize: 16),
+                            )
+                          : Container(),
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: widget.isCollapsable,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: RotationTransition(
+                      turns: Tween<double>(
+                        begin: 0.0,
+                        end: 0.5,
+                      ).animate(_iconController!),
+                      child: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: widget.hasError ? Colors.red : Colors.grey[900],
+                        size: 30,
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
         const Padding(padding: EdgeInsets.symmetric(vertical: 8.0)),
         AnimatedSize(
           duration: const Duration(milliseconds: 450),
           curve: Curves.fastEaseInToSlowEaseOut,
-          child: collapsed && !widget.hasError && widget.isCollapsable
+          child: collapsed && widget.isCollapsable
               ? Container()
               : Column(
                   children:
